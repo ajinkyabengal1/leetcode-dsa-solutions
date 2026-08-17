97b092778c11faebfdec8a197ccab1db65afab3b
Ajinkya Bengal <70565474+ajinkyabengal1@users.noreply.github.com>
2026-08-17 10:16:20 +0530

    Time: 45 ms (79.95%), Space: 56.5 MB (53.66%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let value = init;
+
+    return {
+        increment(){
+            value+= 1;
+            return value
+        },
+        decrement(){
+            value-=1;
+            return value;
+        },
+        reset(){
+            value = init
+            return value
+        }
+    }
+};
