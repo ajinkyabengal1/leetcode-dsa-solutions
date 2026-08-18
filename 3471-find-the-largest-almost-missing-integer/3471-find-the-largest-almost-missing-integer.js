0269f5afd8a40cb346919b6252b1c92d81878d52
Ajinkya Bengal <70565474+ajinkyabengal1@users.noreply.github.com>
2026-08-18 10:21:34 +0530

    Time: 4 ms (34.78%), Space: 57.5 MB (52.17%) - LeetHub

---
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var largestInteger = function(nums, k) {
+   const count = new Map();
+
+    for (let i = 0; i <= nums.length - k; i++) {
+        const unique = new Set();
+
+        for (let j = i; j < i + k; j++) {
+            unique.add(nums[j]);
+        }
+
+        for (const num of unique) {
+            count.set(num, (count.get(num) || 0) + 1);
+        }
+    }
+
+    let ans = -1;
+
+    for (const [num, freq] of count) {
+        if (freq === 1) {
+            ans = Math.max(ans, num);
+        }
+    }
+
+    return ans;
+};
